d14bfefca7e19711a7dcfdb2846f4b1f08129722
hai0z <hainguyen56211@gmail.com>
2022-12-20 10:56:34 +0700

    fix login

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -225,7 +225,7 @@ const SignUp = () => {
                                 <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                                     Already have an account?
                                     <Link
-                                        to="/"
+                                        to="/login"
                                         className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                                     >
                                         {" "}
